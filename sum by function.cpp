2e6caf8ce2c9f5051476b8fc47b2363114208088
yaminimahitha <84062507+yaminimahitha@users.noreply.github.com>
2021-06-04 11:07:30 +0530

    Add files via upload

---
+++ sum by function.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(int a,int b)
+{
+	int c;
+	c=a+b;
+	cout<<"sum= "<<c;
+}
+int main()
+{
+	int a,b,c;
+	cout<<"Enter a number1"<<"\n";
+	cin>>a;
+	cout<<"Enter a number2"<<"\n";
+	cin>>b;
+	c=add(a,b);
+}
